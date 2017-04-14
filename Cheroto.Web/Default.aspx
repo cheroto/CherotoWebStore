9d811462590cd6d96a86bf0637246e8696a2d6b0
Pedro <pedro.cheroto@gmail.com>
2017-04-14 17:08:36 -0400

    First commit for my WebStore!

---
+++ Cheroto.Web/Default.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Cheroto.Web.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Hello World!
+
+            <asp:TextBox runat="server" ID="txtNumber"></asp:TextBox>
+            <asp:Button runat="server" ID="btnSubmit" OnClick="btnSubmit_Click" Text ="Do not click!"/>
+            <asp:Literal runat ="server" ID="ltrHelloWorld"></asp:Literal>
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ Cheroto.Web/Default.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Cheroto.Web
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("<br/>");
+            int i0 = Convert.ToInt32(txtNumber.Text);
+            for (int i = i0; i < i0 + 10; i++)
+            {
+                sb = sb.Append(i.ToString() + "<br/>");
+            }
+            ltrHelloWorld.Text = sb.ToString();
+        }
+    }
+}
