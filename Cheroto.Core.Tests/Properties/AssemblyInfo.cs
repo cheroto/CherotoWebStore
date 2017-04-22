a99001d1ff846b2c4c4a9cbd87b438e77325029a
Pedro <pedro.cheroto@gmail.com>
2017-04-22 18:15:42 -0400

    Test Class added

---
+++ Cheroto.Core.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Cheroto.Core.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Cheroto.Core.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2baa1c44-5416-419e-a20e-501062a74242")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Cheroto.Core.Tests/RGVerificationDigitCalculatorTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using NUnit.Framework;
+using Cheroto.Core;
+
+namespace Cheroto.Core.Tests
+{
+    [TestFixture]
+    public class RGVerificationDigitCalculatorTests
+    {
+        [Test]
+        [TestCase("46621468", "6")]
+        [TestCase("10907440", "3")]
+        [TestCase("45261246", "9")]
+        public void ResultVerifier()
+        {
+            //Arrange
+            var input = "20077193";
+            var expectedOutput = "0";
+            var verifier = new RGVerificationDigitCalculator();
+
+            //Act
+            var output = verifier.CalculateVerificationDigit(input);
+
+            //Assert
+            Assert.AreEqual(output, expectedOutput);
+
+        }
+    }
+}
